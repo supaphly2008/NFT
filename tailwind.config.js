4ff2277fcde25186ba5762ee2642901dd24dd347
Terry <liaoiyang@gmail.com>
2022-04-03 00:29:46 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    height: {
+      screen: "calc(var(--vh) * 100)",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
