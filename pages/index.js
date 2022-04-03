097d734a12c048bb5a5a56a76ce0553b2c2e0e28
Terry <liaoiyang@gmail.com>
2022-04-03 18:58:36 +0800

    add mint function

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,73 @@
-import { useAddress, useDisconnect, useMetamask } from "@thirdweb-dev/react";
+import { useState, useEffect } from "react";
+import { useAddress, useDisconnect, useMetamask, useNFTDrop } from "@thirdweb-dev/react";
+
+const collectionAddress = "0xD5bAD0df8B7449cF6dbE7a83ed55d666a08a63dd";
 
 export default function Home() {
   const address = useAddress();
   const disconnect = useDisconnect();
   const connectWithMetamask = useMetamask();
 
+  const nftDrop = useNFTDrop(collectionAddress);
+
+  const [claimed, setClaimed] = useState(0);
+  const [princeInEth, setPriceInEth] = useState();
+  const [totalSupply, setTotalSupply] = useState(0);
+  const [loading, setLoading] = useState(false);
+
   const onConnectClick = () => {
     address ? disconnect() : connectWithMetamask();
+  };
+
+  useEffect(() => {
+    if (!nftDrop) return;
+
+    const fetchPrice = async () => {
+      const claimedConditions = await nftDrop.claimConditions.getAll();
+      setPriceInEth(claimedConditions?.[0].currencyMetadata.displayValue);
+    };
+
+    fetchPrice();
+  }, [nftDrop]);
+
+  useEffect(() => {
+    if (!nftDrop) return;
+
+    const fetchNFTDropData = async () => {
+      setLoading(true);
+      const claimed = await nftDrop.getAllClaimed();
+      const total = await nftDrop.totalSupply();
+      setClaimed(claimed.length);
+      setTotalSupply(total);
+      setLoading(false);
+    };
+    fetchNFTDropData();
+  }, [nftDrop]);
+
+  const mintNft = () => {
+    if (!nftDrop || !address) return;
+
+    const quantity = 1;
+
+    setLoading(true);
+
+    nftDrop
+      .claimTo(address, quantity)
+      .then(async (tx) => {
+        const receipt = tx[0].receipt;
+        const claimedTokenId = tx[0].id;
+        const claimedNft = await tx[0].data();
+
+        console.log("receipt", receipt);
+        console.log("claimedTokenId", claimedTokenId);
+        console.log("claimedNft", claimedNft);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   };
 
   return (
@@ -46,11 +107,21 @@ export default function Home() {
         <div className="mt-10 flex flex-1 flex-col items-center space-y-6 text-center lg:space-y-0 lg:justify-center">
           <img className="w-80 object-cover pb-10 lg:h-40" src="/Azuki-wall.jpeg" alt="" />
           <h1 className="text-3xl font-bold lg:text-5xl lg:font-extrabold">The Azuki coding club | NFT Drop</h1>
-          <p className="pt-2 text-xl text-green-500">333 / 10,000 of NFTs Minted</p>
+          {loading ? (
+            <p className="pt-2 text-xl text-green-500 animate-bounce">Fetching total supply...</p>
+          ) : (
+            <p className="pt-2 text-xl text-green-500">
+              {claimed} / {totalSupply.toString()} of NFTs Minted
+            </p>
+          )}
+
+          {loading && <img className="w-[200px] h-[200px] object-contain" src="https://cdn.dribbble.com/users/563824/screenshots/3633228/media/b620ccb3ae8c14ea5447d159ebb1da58.gif" />}
         </div>
 
         {/* mint button */}
-        <button className="h-[42px] mt-10 w-full rounded-full bg-red-600 text-white font-bold">Mint NFT (0.25 ETH)</button>
+        <button onClick={mintNft} disabled={loading || claimed === Number(totalSupply) || !address} className="h-[42px] mt-10 w-full rounded-full bg-red-600 text-white font-bold disabled:bg-gray-400">
+          {loading ? <>Loading...</> : claimed === Number(totalSupply) ? <>SOLD OUT</> : !address ? <>Connect wallet to Mint</> : <span>Mint NFT ({princeInEth} ETH)</span>}
+        </button>
       </div>
     </div>
   );
